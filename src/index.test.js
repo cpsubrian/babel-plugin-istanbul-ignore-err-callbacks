9141bbf10d921247a03b27c1fbd7236ab984e640
Brian Link <cpsubrian@gmail.com>
2017-02-22 21:41:56 -0800

    More tests.

--- src/index.test.js
+++ src/index.test.js
@@ -12,16 +12,42 @@ test('adds ignore comment for error callback', () => {
   const code = stripIndent`
     foo(function (err) {
       if (err) return cb(err);
-      bar();
+      bar(err);
     });
   `
   const expected = stripIndent`
     foo(function (err) {
       /* istanbul ignore if */if (err) return cb(err);
-      bar();
+      bar(err);
     });
   `
-  const transformed = transform(code, transformOpts)
+  expect(transform(code, transformOpts).code).toBe(expected)
+})
 
-  expect(transformed.code).toBe(expected)
+test('does not transform non-returning error callbacks', () => {
+  const code = stripIndent`
+    foo(function (err) {
+      if (err) bar(err);
+    });
+  `
+  const expected = stripIndent`
+    foo(function (err) {
+      if (err) bar(err);
+    });
+  `
+  expect(transform(code, transformOpts).code).toBe(expected)
+})
+
+test('does not transform returning callbacks that have an error', () => {
+  const code = stripIndent`
+    foo(function (err) {
+      if (true) return bar(err);
+    });
+  `
+  const expected = stripIndent`
+    foo(function (err) {
+      if (true) return bar(err);
+    });
+  `
+  expect(transform(code, transformOpts).code).toBe(expected)
 })
